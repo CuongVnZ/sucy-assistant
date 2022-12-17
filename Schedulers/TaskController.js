8c13ec23c48336aa7dbb1b14f81a761c0ff5bbe8
CuongVnZ <edgcuong@gmail.com>
2022-12-17 22:28:36 +0700

    update

--- Schedulers/TaskController.js
+++
@@ -1,8 +0,0 @@
-const OnlineCounting = require('./tasks/OnlineCounting');
-
-module.exports = (Discord, client) => {
-    //setup other tasks
-
-    //add OnlineCounting task
-    OnlineCounting(Discord, client);
-}
